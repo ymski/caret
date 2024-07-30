72231fc9e7ddf2359055e22f870ac97ea29c9ab0
Koudai Yamasaki <114902604+ymski@users.noreply.github.com>
2024-07-30 20:50:01 +0900

    update pip args

--- setup_caret.sh
+++ setup_caret.sh
@@ -97,12 +97,12 @@ fi
 # Run ansible if confirmed
 # Install ansible
 # TODO: To update Ansible 6.*
-ansible_version=$(pip3 list | grep -oP "^ansible\s+\K([0-9]+)" || true)
-if [ "$ansible_version" != "5" ]; then
-    sudo apt-get -y update
-    sudo apt-get -y purge ansible
-    sudo pip3 install -U "ansible==5.*"
-fi
+# ansible_version=$(pip3 list | grep -oP "^ansible\s+\K([0-9]+)" || true)
+# if [ "$ansible_version" != "5" ]; then
+#     sudo apt-get -y update
+#     sudo apt-get -y purge ansible
+#     sudo pip3 install -U "ansible==5.*"
+# fi
 
 # Set options
 if [ $noninteractive -eq 0 ]; then
